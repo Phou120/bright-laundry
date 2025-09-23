596422e4ce8476a54ee0b68259e085d5158ba226
phoud <phoud.dev.20@gmail.com>
2025-09-23 22:59:42 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { I18nMiddleware, I18nValidationPipe } from 'nestjs-i18n';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*' });
+  // app.enableCors({ origin: '*' });
+  app.enableCors('*');
   app.setGlobalPrefix('api');
 
   app.use(I18nMiddleware);
