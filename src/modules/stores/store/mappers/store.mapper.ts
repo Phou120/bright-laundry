ed9ca7d55edae7bde15747ff99e10e98e486a870
phoud <phoud.dev.20@gmail.com>
2025-10-09 23:33:48 +0700

    update

--- src/modules/stores/store/mappers/store.mapper.ts
+++ src/modules/stores/store/mappers/store.mapper.ts
@@ -34,7 +34,7 @@ export class StoreDataAccessMapper {
     ormEntity.description = dto.description;
     ormEntity.policy = dto.policy;
     ormEntity.village_id = dto.village_id;
-    ormEntity.store_status_id = EnumStoreStatus.PENDING;
+    ormEntity.store_status_id = EnumStoreStatus.OPEN;
 
     if (method === OrmEntityMethod.CREATE) {
       if (tax_id) {
